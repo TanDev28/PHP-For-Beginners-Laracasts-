1dda8bd70d03b7f91099fa00d2559640f81dfa3c
TanDev28 <tan01101it@gmail.com>
2026-05-29 00:29:40 +0700

    Conditionals and Booleans

--- index.php
+++ index.php
@@ -5,18 +5,35 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <style>
+    body {
+        display: grid;
+        place-items: center;
+        height: 100vh;
+        margin: 0;
+        font-family: sans-serif;
+    }
+    </style>
 </head>
 
 <body>
 
     <?php
 
-    $greeting = "Hello";
-    // echo "$greeting World!";
-    // echo $greeting . " " . "World!";
-    echo '$greeting World!';
+    $name = "Lập trình PHP";
+    $read = true;
+    if ($read) {
+        $message = "Tôi đã đọc cuốn $name";
+    } else {
+        $message = "Tôi CHƯA đọc cuốn $name";
+    }
 
     ?>
+
+    <h1>
+        <!-- Không cần dấu chấm phẩy -->
+        <?= $message ?>
+    </h1>
 
 
 </body>
