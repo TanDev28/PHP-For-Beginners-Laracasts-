99b2389b997d7c0f8e1a9b5fabe31e0b4f7a1f58
TanDev28 <tan01101it@gmail.com>
2026-05-29 16:39:19 +0700

    Lambda Functions 4

--- index.php
+++ index.php
@@ -34,20 +34,7 @@
         ]
     ];
 
-    function filter($items, $fn)
-    {
-        $filteredItems = [];
-
-        foreach ($items as $item) {
-            if ($fn($item)) {
-                $filteredItems[] = $item;
-            }
-        }
-
-        return $filteredItems;
-    }
-
-    $filteredBooks = filter($books, function ($book) {
+    $filteredBooks = array_filter($books, function ($book) {
         return $book['releaseYear'] < 2000;
     });
     ?>
