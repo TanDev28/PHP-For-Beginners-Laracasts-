7cd2cc642d69ac28502a9916493714c1c1922884
TanDev28 <tan01101it@gmail.com>
2026-05-29 16:52:34 +0700

    Separate Logic From the Template

--- index.php
+++ index.php
@@ -1,54 +1,27 @@
-<!DOCTYPE html>
-<html lang="en">
+<?php
+$books = [
+    [
+        'name' => 'Số đỏ',
+        'author' => 'Vũ Trọng Phụng',
+        'releaseYear' => 1936,
+        'purchaseUrl' => 'https://vnexpress.net/'
+    ],
+    [
+        'name' => 'Nỗi buồn chiến tranh',
+        'author' => 'Bảo Ninh',
+        'releaseYear' => 1990,
+        'purchaseUrl' => 'https://vnexpress.net/'
+    ],
+    [
+        'name' => 'Tuổi thơ dữ dội',
+        'author' => 'Phùng Quán',
+        'releaseYear' => 1988,
+        'purchaseUrl' => 'https://vnexpress.net/'
+    ]
+];
 
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+$filteredBooks = array_filter($books, function ($book) {
+    return $book['releaseYear'] < 1990;
+});
 
-</head>
-
-<body>
-
-    <h1>Các cuốn sách văn học</h1>
-
-    <?php
-    $books = [
-        [
-            'name' => 'Số đỏ',
-            'author' => 'Vũ Trọng Phụng',
-            'releaseYear' => 1936,
-            'purchaseUrl' => 'https://vnexpress.net/'
-        ],
-        [
-            'name' => 'Nỗi buồn chiến tranh',
-            'author' => 'Bảo Ninh',
-            'releaseYear' => 1990,
-            'purchaseUrl' => 'https://vnexpress.net/'
-        ],
-        [
-            'name' => 'Tuổi thơ dữ dội',
-            'author' => 'Phùng Quán',
-            'releaseYear' => 1988,
-            'purchaseUrl' => 'https://vnexpress.net/'
-        ]
-    ];
-
-    $filteredBooks = array_filter($books, function ($book) {
-        return $book['releaseYear'] < 2000;
-    });
-    ?>
-
-    <ul>
-        <?php foreach ($filteredBooks as $book) : ?>
-        <a href="<?= $book['purchaseUrl'] ?>">
-            <li>
-                <?= $book['name'] ?> (<?= $book['releaseYear'] ?>) - By <?= $book['author'] ?>
-            </li>
-        </a>
-        <?php endforeach; ?>
-    </ul>
-
-</body>
-
-</html>
+require "index.view.php";
