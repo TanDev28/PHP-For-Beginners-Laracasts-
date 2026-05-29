74e2bb954d773f5e2eb045c618e051585f4ddaec
TanDev28 <tan01101it@gmail.com>
2026-05-29 08:57:58 +0700

    Associative Arrays

--- index.php
+++ index.php
@@ -13,31 +13,37 @@
     <h1>Các cuốn sách văn học</h1>
 
     <?php
+    // $books = [
+    //     "Số đỏ",
+    //     "Nỗi buồn chiến tranh",
+    //     "Tuổi thơ dữ dội"
+    // ];
+    // echo $books[2]; //Truy cập mảng chỉ số
+
     $books = [
-        "Số đỏ",
-        "Nỗi buồn chiến tranh",
-        "Tuổi thơ dữ dội"
+        [
+            'name' => 'Số đỏ',
+            'author' => 'Vũ Trọng Phụng',
+            'purchaseUrl' => 'https://vnexpress.net/'
+        ],
+        [
+            'name' => 'Nỗi buồn chiến tranh',
+            'author' => 'Bảo Ninh',
+            'purchaseUrl' => 'https://vnexpress.net/'
+        ]
     ];
     ?>
 
-    <h2>Cách 1</h2>
-    <!-- Cách 1 -->
-    <ul>
-        <?php
-        foreach ($books as $book) {
-            echo "<li>$book</li>";
-            // echo "<li>{$book}TM</li>"; //Dùng `{}` để cô lập biến khi nối thêm text.
-        }
-        ?>
-    </ul>
-
-    <h2>Cách 2</h2>
-    <!-- Cách 2 -->
     <ul>
         <?php foreach ($books as $book) : ?>
-        <li><?= $book ?></li>
+        <a href="<?= $book['purchaseUrl'] ?>">
+            <li>
+                <?= $book['name'] ?>
+            </li>
+        </a>
         <?php endforeach; ?>
     </ul>
+
 
 </body>
 
