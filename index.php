8f55f411234284673717150626a3edf1d1ed59c9
TanDev28 <tan01101it@gmail.com>
2026-05-30 09:02:28 +0700

    Page Links

--- index.php
+++ index.php
@@ -1,27 +1,3 @@
 <?php
-$books = [
-    [
-        'name' => 'Số đỏ',
-        'author' => 'Vũ Trọng Phụng',
-        'releaseYear' => 1936,
-        'purchaseUrl' => 'https://vnexpress.net/'
-    ],
-    [
-        'name' => 'Nỗi buồn chiến tranh',
-        'author' => 'Bảo Ninh',
-        'releaseYear' => 1990,
-        'purchaseUrl' => 'https://vnexpress.net/'
-    ],
-    [
-        'name' => 'Tuổi thơ dữ dội',
-        'author' => 'Phùng Quán',
-        'releaseYear' => 1988,
-        'purchaseUrl' => 'https://vnexpress.net/'
-    ]
-];
-
-$filteredBooks = array_filter($books, function ($book) {
-    return $book['releaseYear'] < 1990;
-});
 
 require "index.view.php";
