277640a9efa5ad7a654a646e749b5ae1f6335203
TanDev28 <tan01101it@gmail.com>
2026-05-31 11:01:50 +0700

    Make a PHP Router

---
+++ router.php
@@ -0,0 +1,34 @@
+<?php
+$uri = parse_url($_SERVER['REQUEST_URI'])['path'];
+
+// if ($uri === "/") {
+//     require "controllers/index.php";
+// } else if ($uri === "/contact") {
+//     require "controllers/contact.php";
+// } else if ($uri === "/about") {
+//     require "controllers/about.php";
+// }
+
+$routes = [
+    "/" => "controllers/index.php",
+    "/contact" => "controllers/contact.php",
+    "/about" => "controllers/about.php"
+];
+
+function routeToController($uri, $routes)
+{
+    if (array_key_exists($uri, $routes)) {
+        require $routes[$uri];
+    } else {
+        abort();
+    }
+}
+
+function abort($code = 404)
+{
+    http_response_code($code);
+    require "views/{$code}.php";
+    die();
+}
+
+routeToController($uri, $routes);
--- views/partials/nav.php
+++ views/partials/nav.php
@@ -11,10 +11,10 @@
                         <!-- Current: "bg-gray-900 text-white", Default: "text-gray-300 hover:bg-white/5 hover:text-white" -->
                         <a href="/" aria-current="page"
                             class="rounded-md px-3 py-2 text-sm font-medium <?= urlIs('/') ? 'bg-gray-900 text-white' : 'text-gray-300 hover:bg-white/5 hover:text-white' ?>">Home</a>
-                        <a href="/about.php"
-                            class="rounded-md px-3 py-2 text-sm font-medium <?= urlIs('/about.php') ? 'bg-gray-900 text-white' : 'text-gray-300 hover:bg-white/5 hover:text-white' ?>">About</a>
-                        <a href="/contact.php"
-                            class="rounded-md px-3 py-2 text-sm font-medium <?= urlIs('/contact.php') ? 'bg-gray-900 text-white' : 'text-gray-300 hover:bg-white/5 hover:text-white' ?>">Contact</a>
+                        <a href="/about"
+                            class="rounded-md px-3 py-2 text-sm font-medium <?= urlIs('/about') ? 'bg-gray-900 text-white' : 'text-gray-300 hover:bg-white/5 hover:text-white' ?>">About</a>
+                        <a href="/contact"
+                            class="rounded-md px-3 py-2 text-sm font-medium <?= urlIs('/contact') ? 'bg-gray-900 text-white' : 'text-gray-300 hover:bg-white/5 hover:text-white' ?>">Contact</a>
 
                     </div>
                 </div>
