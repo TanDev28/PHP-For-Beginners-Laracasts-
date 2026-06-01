25d783c6effb437688c1f774399fc07c95868f35
TanDev28 <tan01101it@gmail.com>
2026-06-01 18:53:54 +0700

    Extract a PHP Database Class

--- index.php
+++ index.php
@@ -3,17 +3,12 @@
 require "functions.php";
 // require "router.php";
 
-// connect to our MySQL database
-$dsn = "mysql:host=localhost;port=3306;dbname=myapp;user=root;charset=utf8mb4";
+require "Database.php";
 
-$pdo = new PDO($dsn);
+$db = new Database();
+// Phân biệt fetch và fetchAll
+// $posts = $db->query("select * FROM posts where id > 1")->fetchAll(PDO::FETCH_ASSOC);
+// $posts = $db->query("select * FROM posts where id > 1")->fetch(PDO::FETCH_ASSOC);
+$posts = $db->query("select * FROM posts")->fetchAll(PDO::FETCH_ASSOC);
 
-$statement = $pdo->prepare("select * FROM posts");
-$statement->execute();
-
-// $posts = $statement->fetchAll(); // Bị trùng lặp
-$posts = $statement->fetchAll(PDO::FETCH_ASSOC); // Xử lý trùng lặp
-
-foreach ($posts as $post) {
-    echo "<li>" . $post['title'] . "</li>";
-}
+dd($posts);
