f543c1c70c3ba4f121c7465fb2184cb81868976c
TanDev28 <tan01101it@gmail.com>
2026-06-01 11:13:15 +0700

    PDO First Steps 1

--- index.php
+++ index.php
@@ -1,5 +1,25 @@
 <?php
 
 require "functions.php";
+// require "router.php";
 
-require "router.php";
+class Person
+{
+    public $name;
+    public $age;
+
+    public function breathe()
+    {
+        echo "$this->name is breathing";
+    }
+}
+
+$person = new Person();
+$person->name = "Tân";
+$person->age = 25;
+
+// dd($person);
+// dd($person->name);
+// dd($person->age);
+// dd($person->breathe());
+$person->breathe();
