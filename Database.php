237f530d2a50b8455bd1a44bb4366769ec8f106c
TanDev28 <tan01101it@gmail.com>
2026-06-02 08:39:56 +0700

    Environments and Configuration Flexibility 1

--- Database.php
+++ Database.php
@@ -3,11 +3,24 @@ class Database
 {
     public $connection;
 
-    public function __construct()
+    public function __construct($config)
     {
-        $dsn = "mysql:host=localhost;port=3306;dbname=myapp;user=root;charset=utf8mb4";
+        // Đẩy $config qua index.php
+        // $config = [
+        //     "host" => "localhost",
+        //     "port" => 3306,
+        //     "dbname" => "myapp",
+        //     "charset" => "utf8mb4"
+        // ];
 
-        $this->connection = new PDO($dsn);
+        // $dsn = "mysql:host={$config['host']};port={$config['port']};dbname={$config['dbname']};charset={$config['charset']}";
+        // Khắc phục gõ $dsn thủ công bằng // dd(http_build_query($config, "", ";"));
+        $dsn = "mysql:" . http_build_query($config, "", ";");
+
+
+        $this->connection = new PDO($dsn, "root", "", [
+            PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC
+        ]);
     }
 
     public function query($query)
--- index.php
+++ index.php
@@ -5,10 +5,15 @@ require "functions.php";
 
 require "Database.php";
 
-$db = new Database();
-// Phân biệt fetch và fetchAll
-// $posts = $db->query("select * FROM posts where id > 1")->fetchAll(PDO::FETCH_ASSOC);
-// $posts = $db->query("select * FROM posts where id > 1")->fetch(PDO::FETCH_ASSOC);
-$posts = $db->query("select * FROM posts")->fetchAll(PDO::FETCH_ASSOC);
+$config = [
+    "host" => "localhost",
+    "port" => 3306,
+    "dbname" => "myapp",
+    "charset" => "utf8mb4"
+];
+
+$db = new Database($config);
+
+$posts = $db->query("select * FROM posts")->fetchAll();
 
 dd($posts);
