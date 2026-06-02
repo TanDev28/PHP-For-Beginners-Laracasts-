db83ac468c20056a4d74840b42828dcc3dae137e
TanDev28 <tan01101it@gmail.com>
2026-06-02 16:35:43 +0700

    SQL Injection Vulnerabilities Explained

--- index.php
+++ index.php
@@ -6,9 +6,17 @@ require "functions.php";
 require "Database.php";
 
 $config = require "config.php";
-
 $db = new Database($config["database"]);
 
-$posts = $db->query("select * FROM posts")->fetchAll();
+$id = $_GET['id'];
+// $query = "select * FROM posts where id = {$id}"; //Sự nguy hiểm của chèn trực tiếp như này, hacker có thể chèn thêm drop table
+
+// Cách khắc phục 1 
+// $query = "select * FROM posts where id = ?";
+// $posts = $db->query($query, [$id])->fetch();
+
+// Cách khắc phục 2
+$query = "select * FROM posts where id = :id";
+$posts = $db->query($query, ["id" => $id])->fetch();
 
 dd($posts);
