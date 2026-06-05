6fe93ff35c8245803e029742ad24faef977bba58
TanDev28 <tan01101it@gmail.com>
2026-06-05 12:27:57 +0700

    Extract a Simple Validator Class

--- controllers/note-create.php
+++ controllers/note-create.php
@@ -1,19 +1,19 @@
 <?php
 
+require "Validator.php";
+
 $config = require "config.php";
 $db = new Database($config["database"]);
+
+// dd(Validator::email('tan@gmail.com')); // Trả về false nếu sai, ngược lại trả về chính định dạng email đúng tan@gmail.com
 
 $heading = "Create Notes";
 
 if ($_SERVER['REQUEST_METHOD'] == "POST") {
 
     $errors = [];
-    if (strlen($_POST['body']) == 0) {
-        $errors['body'] = "A body is required.";
-    }
-
-    if (strlen($_POST['body']) > 50) {
-        $errors['body'] = "A body can not more than 50 characters.";
+    if (! Validator::string($_POST['body'], 1, 50)) {
+        $errors['body'] = "The body of no more than 50 characters is required.";
     }
 
     if (empty($errors)) {
