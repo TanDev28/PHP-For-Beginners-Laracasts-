1e539c9b38b1c5b40e448d891c620af1a2e0f545
TanDev28 <tan01101it@gmail.com>
2026-06-05 16:38:22 +0700

    Resourceful Naming Conventions

--- views/notes/create.view.php
+++ views/notes/create.view.php
@@ -1,6 +1,6 @@
-<?php require("partials/head.php") ?>
-<?php require("partials/nav.php") ?>
-<?php require("partials/banner.php") ?>
+<?php require("views/partials/head.php") ?>
+<?php require("views/partials/nav.php") ?>
+<?php require("views/partials/banner.php") ?>
 
 <main>
     <div class="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8">
@@ -16,7 +16,7 @@
                                     class="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6"><?= $_POST['body'] ?? '' ?></textarea>
 
                                 <?php if (isset($errors['body'])) : ?>
-                                <p class="text-red-500 text-xs mt-2"><?= $errors['body'] ?></p>
+                                    <p class="text-red-500 text-xs mt-2"><?= $errors['body'] ?></p>
                                 <?php endif; ?>
                             </div>
 
@@ -36,4 +36,4 @@
     </div>
 </main>
 
-<?php require("partials/footer.php") ?>
+<?php require("views/partials/footer.php") ?>
