ac7a5dffad949f00df241c674ef1663db7c75d05
TanDev28 <tan01101it@gmail.com>
2026-06-07 21:38:36 +0700

    Namespacing: What, Why, How?

--- public/index.php
+++ public/index.php
@@ -2,10 +2,12 @@
 
 const BASE_PATH = __DIR__ . "/../"; // Lấy đường dẫn thư mục gốc (hiện tại là public) xong lùi ra một thư mục
 
-require BASE_PATH . "functions.php";
+require BASE_PATH . "Core/functions.php";
 
 spl_autoload_register(function ($class) {
-    require base_path("Core/{$class}.php");
+    // dd($class); // Xem lại $class khi dùng namespace Core\Database
+    $class = str_replace('\\', DIRECTORY_SEPARATOR, $class);
+    require base_path("{$class}.php");
 });
 
-require base_path("router.php");
+require base_path("Core/router.php");
