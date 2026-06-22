bb4c452a70f4fc21c80ab6243943d5b13d02ec3e
TanDev28 <tan01101it@gmail.com>
2026-06-22 23:15:26 +0700

    Composer and Free Autoloading

--- public/index.php
+++ public/index.php
@@ -3,17 +3,19 @@
 use Core\ValidationException;
 use Core\Session;
 
-session_start();
-
 const BASE_PATH = __DIR__ . "/../"; // Lấy đường dẫn thư mục gốc (hiện tại là public) xong lùi ra một thư mục
+require BASE_PATH . "vendor/autoload.php";
+
+
+session_start();
 
 require BASE_PATH . "Core/functions.php";
 
-spl_autoload_register(function ($class) {
-    // dd($class); // Xem lại $class khi dùng namespace Core\Database
-    $class = str_replace('\\', DIRECTORY_SEPARATOR, $class);
-    require base_path("{$class}.php");
-});
+// spl_autoload_register(function ($class) {
+//     // dd($class); // Xem lại $class khi dùng namespace Core\Database
+//     $class = str_replace('\\', DIRECTORY_SEPARATOR, $class);
+//     require base_path("{$class}.php");
+// });
 
 require base_path('bootstrap.php');
 
